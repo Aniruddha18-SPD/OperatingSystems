6d16f6ff5130ddb9edae5d220270af3ac1ea980b
Aniruddha Pokhrel <aniruddha.pokhrel@bison.howard.edu>
2022-10-16 17:44:39 +0000

    bug fixes

--- Processes and Signals/Signals/timer.c
+++ Processes and Signals/Signals/timer.c
@@ -7,11 +7,6 @@
 int x=0;
 int count=0;
 
-void time (int signum){
-  printf("The program ran for %d seconds \n",count);
-  exit(1);
-}
-
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
@@ -19,15 +14,21 @@ void handler(int signum)
   count+=1;
 }
 
+void time (int signum){
+  printf("The program ran for %d seconds \n",count);
+  exit(1);
+}
+
+
 int main(int argc, char * argv[])
 {
   while(1){
     signal(SIGINT,time);
-    signal(SIGALRM,handler); //register handler to handle SIGALRM
-    alarm(1); //Schedule a SIGALRM for 1 second
-    while(x==0); //busy wait for signal to be delivered
+    signal(SIGALRM,handler); 
+    alarm(1); /
+    while(x==0); 
     printf("Turing was right!\n");
     x=0;
   }
-  return 0; //never reached
+  return 0; 
 }
