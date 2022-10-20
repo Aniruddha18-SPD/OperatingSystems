5ab8d0ceebb4e8371cd9b6116ccac07593bc939c
Aniruddha Pokhrel <aniruddha.pokhrel@bison.howard.edu>
2022-10-20 05:05:26 +0000

    changes made to code

--- list/list.h
+++ list/list.h
@@ -2,7 +2,7 @@
 // 
 // Interface definition for linked list.
 //
-// <Author>
+// <Anirudha Pokhrel>
 
 #include <stdbool.h>
 
@@ -10,9 +10,9 @@
  * you want! */
 typedef int elem;
 
-/* Defines the node structure. Each node contains its element, and points to the
- * next node in the list. The last element in the list should have NULL as its
- * next pointer. */
+/*Defines the node of a object. Each node contains the elements and pointer to
+point at the next node in the list. The last element has NULL as its pointer 
+signifying no node after that node. */
 struct node {
 	elem value;
 	struct node *next;
@@ -32,27 +32,29 @@ typedef struct list list_t;
 list_t *list_alloc();
 void list_free(list_t *l);
 
-/* Prints the list in some format. */
-void list_print(list_t *l);
-
 /* Returns the length of the list. */
 int list_length(list_t *l);
+
+/* Prints the list in some format. */
+void list_print(list_t *l);
 
 /* Methods for adding to the list. */
 void list_add_to_back(list_t *l, elem value);
 void list_add_to_front(list_t *l, elem value);
-void list_add_at_index(list_t *l, elem value, int index);  // adds to end of list if out of bounds
+void list_add_at_index(list_t *l, elem value, int index);
 
-/* Methods for removing from the list. Returns the removed element, or -1 for empty list */
-elem list_remove_from_back(list_t *l);
+/* Methods for removing from the list. Returns the removed element. */
 elem list_remove_from_front(list_t *l);
+elem list_remove_from_back(list_t *l);
 elem list_remove_at_index(list_t *l, int index);
 
 /* Checks to see if the given element exists in the list. */
 bool list_is_in(list_t *l, elem value);
 
-/* Returns the element at the given index, or -1 if empty or out of bounds */
+/* Returns the index of element where it occurs */
+int list_get_index_of(list_t *l, elem value);
+
+/* Returns the element at the given index. */
 elem list_get_elem_at(list_t *l, int index);
 
-/* Returns the index at which the given element appears, or -1 if not found */
-int list_get_index_of(list_t *l, elem value);
+/* Returns the index at which the given element appears. */
--- list/main.c
+++ list/main.c
@@ -1,80 +1,33 @@
 #include <stdio.h>
-
-#include "list.c"
+#include <stdlib.h>
+#include "list.h"
 
 int main() {
-  printf("Tests for linked list\n");
-
-  list_t* list = list_alloc();
-  // add to front tests
-  list_add_to_front(list, 1);
-  list_add_to_front(list, 2);
-  list_add_to_front(list, 3);
-  list_add_to_front(list, 4);
-  list_print(list);
-  
-  // add to back tests
-  list_add_to_back(list, 13);
-  list_add_to_back(list, 14);
-  list_add_to_back(list, 15);
-  list_add_to_back(list, 16);
-  list_print(list);
-  
-  // add at index
-  list_add_at_index(list, 11, 4);
-  list_add_at_index(list, 12, 5);
-  list_add_at_index(list, 12, 50);
-  list_add_at_index(list, 12, -1);
-
-  list_print(list);
-  
-  // remove from back tests
-  printf("%d\n", list_remove_from_back(list));
-  printf("%d\n", list_remove_from_back(list));
-  
-  list_print(list);
-  
-  // remove from front tests
-  printf("%d\n", list_remove_from_front(list));
-  printf("%d\n", list_remove_from_front(list));
-  
-  list_print(list);
-  
-  // remove from index tests
-  printf("%d\n", list_remove_at_index(list, 2));
-  printf("%d\n", list_remove_at_index(list, 2));
-  
-  list_print(list);
-  
-  // is in list tests
-  printf("%s\n", list_is_in(list, 0) ? "true" : "false");
-  printf("%s\n", list_is_in(list, 10) ? "true" : "false");
-  printf("%s\n", list_is_in(list, 1) ? "true" : "false");
-  printf("%s\n", list_is_in(list, 12) ? "true" : "false");
-  
-  list_print(list);
-  
-  // get at index tests
-  printf("%d\n", list_get_elem_at(list, 0));
-  printf("%d\n", list_get_elem_at(list, 1) );
-  printf("%d\n", list_get_elem_at(list, 3));
-  printf("%d\n", list_get_elem_at(list, -10));
-  printf("%d\n", list_get_elem_at(list, 10));
-  
-  list_print(list);
-  
-  // get index of tests
-  printf("%d\n", list_get_index_of(list, 9));
-  printf("%d\n", list_get_index_of(list, 10) );
-  printf("%d\n", list_get_index_of(list, 11));
-  printf("%d\n", list_get_index_of(list, 12));
-  printf("%d\n", list_get_index_of(list, 13));
-  
-  list_print(list);
-  
-  list_free(list);
-  list_print(list);
-  
-  
+  printf("Test cases for linked list implementation\n");
+  list_t* a = list_alloc();
+  list_add_to_front(a,1);
+  list_add_to_front(a,3);
+  list_add_to_back(a,2);
+  list_add_at_index(a,4,1);
+  list_print(a);
+  printf("%d\n",list_length(a));
+  printf("Remove\n");
+  printf("%d\n",list_remove_from_back(a));
+   printf("%d\n",list_remove_from_front(a));
+  printf("%d\n",list_remove_from_back(a));
+  // printf("%d\n",list_remove_from_front(a));
+  printf("%d\n",list_length(a));
+  printf("Check\n");
+  printf("%s\n", list_is_in(a,5)?"True":"False");
+  printf("%s\n", list_is_in(a,4)?"True":"False");
+  printf("%s\n", list_is_in(a,7)?"True":"False");
+  list_add_to_back(a,6);
+  printf("%d\n", list_get_elem_at(a,1));
+  printf("%d\n", list_get_elem_at(a,0));
+  printf("Index\n");
+  printf("%d\n", list_get_index_of(a,6));
+  printf("%d\n", list_get_index_of(a,4));
+  printf("Remove at index\n");
+  printf("%d\n",list_remove_at_index(a,1));
   return 0;
 }
